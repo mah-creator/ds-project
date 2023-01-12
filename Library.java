c3b6ccf2a8ad76ab34dec1e8e8e44e510242d8cb
mah-creator <drivemah.tah@gmail.com>
2023-01-13 01:07:53 +0200

    modified: Library.java

--- Library.java
+++ Library.java
@@ -1,8 +1,16 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class Library {
     private BookDatabase bookDatabase = new BookDatabase();
-
+    private HashMap<User, TreeMap<String, Book>> hashMap = new HashMap();
+    private User activeUser = new User();
+    
     public Book[] getBookList() {
         return bookDatabase.getBookList();
     }
-    
+
+    void add(Book book){
+        hashMap.get(activeUser).put(book.getTitle(), book);
+    }
 }
