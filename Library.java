70fa5d228cb6df3f7feafb326341dba7af04fc2f
mah-creator <drivemah.tah@gmail.com>
2023-01-13 14:16:33 +0200

    modified: Library.java

--- Library.java
+++ Library.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Library {
+    private BookDatabase bookDatabase = new BookDatabase();
+    private HashMap<User, ArrayList<Book>> userBookMap = new HashMap();
+
+    // this defines the user that is currently logged into the library system
+    // where only one user can use the system at a time
+    private User activeUser;
     
+    // returns all books from the BookDatabase
+    Book[] getBookList() {
+        return bookDatabase.getBookList();
+    }
+
+    // adds a book to the logged-in user (activeUser) 
+    void add(Book book){
+        if(userBookMap.containsKey(activeUser))
+            userBookMap.get(activeUser).add(book);
+
+        else if(!userBookMap.containsKey(activeUser)){
+            ArrayList<Book> bookList = new ArrayList();
+            bookList.add(book);
+        }
+    }
+
+    // returns the list of books corresponding to the logged-in user
+    Book[] getUserBooksList(){
+        ArrayList<Book> userBooksList = userBookMap.get(activeUser);
+        return userBooksList.toArray(new Book[userBooksList.size()]);
+    }
 }
