9abedada97bb58583e919cd707c999afce0e27af
mah-creator <drivemah.tah@gmail.com>
2023-01-13 21:53:52 +0200

    --

--- MainController.java
+++ MainController.java
@@ -4,9 +4,9 @@ public class MainController{
     // the initial fxml file to be loaded into the programme
     static final String INITIAL_FXML_FILE = ".\\gui\\login.fxml";
 
-    private Library library = new Library();
-    private Book[] bookList = library.getBookList();
+    protected Library library = new Library();
+    protected Book[] bookList = library.getBookList();
 
     // get the stage from where it was created (the javafx.application.Application class)
-    private Stage primaryStage = App.primaryStage;
+    protected Stage primaryStage = App.primaryStage;   
 }
