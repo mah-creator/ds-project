3c694920afcefc8704d81c62641ef37620e370c9
mah-creator <drivemah.tah@gmail.com>
2023-01-15 01:34:15 +0200

    essential enum classes
    it defines the columns that are sortable and their connection to the hard copy of the book database file

---
+++ SortCriteria.java
@@ -0,0 +1,17 @@
+public enum SortCriteria{
+    BOOK_NAME(0, "A - Z", DatabaseColumn.TITLE),
+    BOOK_AUTHOR(1, "author", DatabaseColumn.AUTHOR),
+    BOOK_PUBLISHER(2, "publisher", DatabaseColumn.PUBLISHER),
+    BOOK_PUBLISH_DATE(3, "publish date", DatabaseColumn.PUBLISH_DATA),
+    BOOK_RATING(4, "rating", DatabaseColumn.RATING);
+
+    final int value;
+    final String sortLabel;
+    final DatabaseColumn corrospondingDatabaseColumn;
+
+    SortCriteria(int value, String sortLabel, DatabaseColumn corrospondingDatabaseColumn){
+        this.value = value;
+        this.sortLabel = sortLabel;
+        this.corrospondingDatabaseColumn =corrospondingDatabaseColumn;
+    }
+}
