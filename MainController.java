dbbfc998253c8cad17838c5cd1e665ee98d9aa83
mah-creator <drivemah.tah@gmail.com>
2023-01-15 01:39:18 +0200

    we have multiple trees for listed books
    each one of these trees is sorted based on different book attribute

--- MainController.java
+++ MainController.java
@@ -5,8 +5,12 @@ public class MainController{
     static final String INITIAL_FXML_FILE = ".\\gui\\login.fxml";
 
     protected Library library = App.library;
-    protected  Book[] bookList = App.bookList;
+    protected  Book[][] bookListsSortedDifferently = App.bookListsSortedDifferently;
 
     // get the stage from where it was created (the javafx.application.Application class)
     protected Stage primaryStage = App.primaryStage;   
+
+    protected Book[] getSortedBooks(int sortCriteria){
+        return bookListsSortedDifferently[sortCriteria];
+    }
 }
